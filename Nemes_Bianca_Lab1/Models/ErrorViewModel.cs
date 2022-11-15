bb4c8f2cd40a40fbb75abcea7a5db3c6ef41b9d3
Liviu <Liviu@DESKTOP-7SMC94T>
2022-11-15 13:13:20 +0200

    Add project files.

---
+++ Nemes_Bianca_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nemes_Bianca_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
