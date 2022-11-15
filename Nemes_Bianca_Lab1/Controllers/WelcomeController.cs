8196d29acbfd8ad6d4689ceb74ed35a93e501e0c
Nemes Bianca Corina <nemes.bianca@yahoo.com>
2022-11-15 14:01:28 +0200

    Prima Pagina modificata

---
+++ Nemes_Bianca_Lab1/Controllers/WelcomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nemes_Bianca_Lab1.Models;
+using System.Diagnostics;
+
+namespace Nemes_Bianca_Lab1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+
+    }
+}
